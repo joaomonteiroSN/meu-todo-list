dedc3409375ffc2c12ddf0cbff196982c4d907e4
joao monteiro <70484799+joaomonteiroSN@users.noreply.github.com>
2025-04-19 23:02:20 -0300

    adcionando funcoes criar tarefa (modal) e fecharModal

---
+++ app.js
@@ -0,0 +1,9 @@
+function criarTarefa() {
+    overlay.classList.add("active")
+    modal.classList.add("active")
+}
+
+function fecharModal() {
+    overlay.classList.remove("active")
+    modal.classList.remove("active")
+}
