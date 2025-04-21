4b18e2a10f87d50368515b0e8d101cda694a2b12
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2025-04-21 17:26:17 -0300

    criacao do package-json e criacao da funcao listar tarefas

--- app.js
+++ app.js
@@ -7,3 +7,30 @@ function fecharModal() {
     overlay.classList.remove("active")
     modal.classList.remove("active")
 }
+
+// Para fazer:
+// criar tarefas
+// deletar tarefa
+// listar tarefas - ok
+// buscar tarefas
+
+function listarTarefas() {
+    fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        res.map(tarefa => {
+            listField.innerHTML += `
+            <li>
+                    <h3>${tarefa.titulo}</h3>
+                    <p>${tarefa.descricao}</p>
+                    <div>
+                        <box-icon type='solid' name='trash-alt' animation='flashing-hover'></box-icon>
+                    </div>
+                </li>
+        `
+        })
+        
+    })
+}
+listarTarefas();
